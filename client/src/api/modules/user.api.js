b21a8f7a67296548f8283366e9755947550e8e7e
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-02-22 10:24:43 -0500

    16.API Service

---
+++ client/src/api/modules/user.api.js
@@ -0,0 +1,62 @@
+import privateClient from '../client/private.client.js';
+import publicClient from '../client/public.client.js';
+
+const userEndpoints = {
+  sigin: 'user/sigin',
+  signup: 'user/signup',
+  getInfo: 'user/info',
+  passwordUpdate: 'user/update-password',
+};
+
+const userApi = {
+  signin: async ({ username, password }) => {
+    try {
+      const response = await publicClient.post(userEndpoints.sigin, {
+        username,
+        password,
+      });
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+
+  signup: async ({ username, password, confirmPassword, displayName }) => {
+    try {
+      const response = await publicClient.post(userEndpoints.signup, {
+        username,
+        password,
+        confirmPassword,
+        displayName,
+      });
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+
+  getInfo: async () => {
+    try {
+      const response = await privateClient.get(userEndpoints.getInfo);
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+
+  passwordUpdate: async ({ password, newPassword, confirmNewPassword }) => {
+    try {
+      const response = await privateClient.put(userEndpoints.signup, {
+        password,
+        newPassword,
+        confirmNewPassword,
+      });
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+};
+
+export default userApi;
