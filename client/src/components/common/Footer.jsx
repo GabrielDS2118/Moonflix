dafd39ae0489bc34c89b8f74db3a26ad89381da0
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-02-24 17:29:59 -0500

    26.Footer

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+
+import { Paper, Stack, Box, Button } from '@mui/material';
+
+import menuConfigs from '../../configs/menu.config.js';
+
+import Container from './Container';
+import Logo from '../common/Logo';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Paper square={true} sx={{ backgroundImage: 'unset', padding: '2rem' }}>
+        <Stack
+          alignItems="center"
+          justifyContent="space-between"
+          direction={{ xs: 'column', md: 'row' }}
+          sx={{ height: 'max-content' }}
+        >
+          <Logo />
+          <Box>
+            {menuConfigs.main.map((item, index) => (
+              <Button
+                key={index}
+                sx={{ color: 'inherit' }}
+                component={Link}
+                to={item.path}
+              >
+                {item.display}
+              </Button>
+            ))}
+          </Box>
+        </Stack>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Footer;
--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,14 +1,21 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import Footer from '../common/Footer';
 import GlobalLoading from '../common/GlobalLoading';
+import TopBar from '../common/TopBar';
 const MainLayout = () => {
   return (
     <>
       {/*global loading*/}
       <GlobalLoading />
+      {/*global loading*/}
+
+      {/*login modal*/}
       {/*login modal*/}
 
       <Box display="flex" minHeight="100vh">
+        {/*header*/}
+        <TopBar />
         {/*header*/}
 
         {/*main*/}
@@ -16,10 +23,11 @@ const MainLayout = () => {
           <Outlet />
         </Box>
         {/*main*/}
-
-        {/*footer*/}
-        {/*footer*/}
       </Box>
+
+      {/*footer*/}
+      <Footer />
+      {/*footer*/}
     </>
   );
 };
