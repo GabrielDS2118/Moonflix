e3eb784de34a6e6e454e226e8c5156b0898d5e2a
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-02 15:47:15 -0500

    43.Person Detail Page

--- server/src/controllers/person.controller.js
+++ server/src/controllers/person.controller.js
@@ -1,4 +1,4 @@
-import responseHandler from '../handlers/request.handler.js';
+import responseHandler from '../handlers/response.handler.js';
 import tmdbApi from '../tmdb/tmdb.api.js';
 
 const personDetail = async (req, res) => {
